f2e9d6e11f57beacf3a8cfd9304816f0001ac1b8
Micah Denbraver <macro1@users.noreply.github.com>
2025-04-27 14:23:00 -0700

    add some missing stubs

--- stubs/grpcio/grpc/aio/__init__.pyi
+++ stubs/grpcio/grpc/aio/__init__.pyi
@@ -6,7 +6,6 @@ from concurrent import futures
 from types import TracebackType
 from typing import Any, Generic, NoReturn, TypeVar, overload, type_check_only
 from typing_extensions import Self, TypeAlias
-from grpc.aio._typing import EOFType
 
 from grpc import (
     CallCredentials,
@@ -21,6 +20,7 @@ from grpc import (
     StatusCode,
     _Options,
 )
+from grpc.aio._typing import EOFType
 
 _TRequest = TypeVar("_TRequest")
 _TResponse = TypeVar("_TResponse")
@@ -155,6 +155,9 @@ class Server(metaclass=abc.ABCMeta):
     # Returns a bool indicates if the operation times out. Timeout is in seconds.
     @abc.abstractmethod
     async def wait_for_termination(self, timeout: float | None = ...) -> bool: ...
+    def add_registered_method_handlers(
+        self, service_name: str, method_handlers: dict[str, RpcMethodHandler[Any, Any]]
+    ) -> None: ...
 
 # Client-Side Context:
 
